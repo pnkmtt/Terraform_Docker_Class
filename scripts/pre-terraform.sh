373329210b3cb65d4fc856cfea6416ab340f2332
pnkmtt <pnkmtt@gmail.com>
2018-02-27 14:08:28 -0500

    wokring build of terraform box
    build works ansible 2.4

--- scripts/pre-terraform.sh
+++ scripts/pre-terraform.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-echo 'copy in ssh keys'
+echo 'copy in lab ssh keys'
 cp /vagrant/files/ssh/id_rsa* ~vagrant/.ssh
 chmod 644 ~vagrant/.ssh/id_rsa*
 cat ~vagrant/.ssh/id_rsa.pub >> ~vagrant/.ssh/authorized_keys
-#echo 'add ansible respoitory'
-#sudo apt-add-repository ppa:ansible/ansible -y
-echo 'update system'
-sudo apt-get update -y
-echo 'install ansible'
-sudo apt-get install software-properties-common python-software-properties ansible -y
+echo 'install ansible 2.4'
+sudo apt update
+#sudo apt upgrade -y
+sudo apt install software-properties-common python-software-properties -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install ansible -y
+ansible --version
+echo 'because of windows copy the inventory file to a directory and fix permissions - this is a hack'
+cp /vagrant/hosts /var/tmp/hosts
+chmod -x /var/tmp/hosts
